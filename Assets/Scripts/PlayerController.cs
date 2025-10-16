c538b7178ad6515978d010aad5ff2d29056a8ceb
aecio@desktop <aeciobrito@gmail.com>
2025-10-16 16:09:29 -0300

    Display score and text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
+    private float _speed = 10.0f;
+    private int _count = 0;
+    public GameObject winTextObject;
+    public TextMeshProUGUI _countText;
     private Rigidbody _rigidbody;
-    private Vector2 movementVector;
+    private Vector2 movementVector = Vector2.zero;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _countText.text = $"Count: {_count}";
+        winTextObject.SetActive(false);
     }
 
     void FixedUpdate()
     {
-        _rigidbody.AddForce(new Vector3(movementVector.x, 0.0f, movementVector.y) * speed);
+        _rigidbody.AddForce(new Vector3(movementVector.x, 0.0f, movementVector.y) * _speed);
     }
 
     void OnMove(InputValue movementValue)
     {
         movementVector = movementValue.Get<Vector2>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            _countText.text = $"Count: {_count}";
+            if (_count >= 12)
+                winTextObject.SetActive(true);
+        }   
     }
 }
